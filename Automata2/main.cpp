b12636007d56e025f5f664bd20fe434eb288a74c
valentinnsh <valentinnsh@gmail.com>
2018-12-10 02:02:53 +0300

    Add new repo for little student tasks. Add org-file, that contains info about repo. Add folder for FSM second task.

---
+++ Automata2/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 V.Shishkin
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+class Automate_c{
+  vector m_stack;
+  
+public:
+};
+
+int main(int argc, char* argv[]){
+  return 0;
+}
